e92accf56021cb200bba20747cd13fade067adf2
Sovannakk <sovannak.khengg@gmail.com>
2024-12-04 21:47:05 +0700

    init

--- src/main/java/com/kshrd/todoservice/client/CategoryClient.java
+++ src/main/java/com/kshrd/todoservice/client/CategoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "category-service", url = "http://localhost:8081/api/v1/categories")
+@FeignClient(name = "category-service", url = "http://139.59.250.62:8070/api/v1/categories")
 public interface CategoryClient {
 
     @GetMapping("/{id}")
